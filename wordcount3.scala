60e1b2c2d0cebcf1a81ef4dd019ed1ceaad554df
Swetha <swe0523@gmail.com>
2014-06-14 11:41:36 +0530

    Create wordcount3.scala

---
+++ wordcount3.scala
@@ -0,0 +1,8 @@
+object count{
+  def main(args: Array[String]) {
+     val fruit = List("apples","apples","apples","orange", "pears","apples","apples","apples","orange","orange","strawberry","apples","apples","orange","orange","apples","strawberry","cherry","cherry","orange","cherry")
+     val word = fruit.flatMap(_.split("\n"))
+     val counter = word.map(word => (word,1)).groupBy(_._1)
+     val reduce = counter.foreach{case(key,value) => println(key ,value.length) }
+   }
+}
